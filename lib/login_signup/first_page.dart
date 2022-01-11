6378e7c2417e84a60f3fa938dcc87f5f34df352a
Emelda <emativenga16@outlook.com>
2022-01-11 09:56:33 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/main.dart

--- lib/login_signup/first_page.dart
+++ lib/login_signup/first_page.dart
@@ -2,8 +2,6 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:google_maps_in_flutter/new_main.dart';
-import 'package:google_maps_in_flutter/main.dart';
 
 void main() {
   runApp(MaterialApp(
